abb47d439cdad1fc293d0b84ee72bbeb768e7ac1
Danila Andreev <mail@neandreev.ru>
2025-03-11 04:23:29 +0500

    update electron app window size

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,8 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 450,
-    height: 300,
+    width: 700,
+    height: 700,
     resizable: false,
     show: false,
     autoHideMenuBar: true,
